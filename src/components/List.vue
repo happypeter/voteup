3b86f2307d3844afebaaad25f915c58a7589a1ec
Peter Wang <happypeter1983@gmail.com>
2017-10-17 14:41:20 +0800

    vote-p2

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,28 +1,21 @@
 <template>
   <div class="list">
     <div class="option" v-for="option in options">
-      <div class="title">
-        <div class="text">
-          {{ option.text }}
-        </div>
-      </div>
-      <div class="votes">
-        <div v-for="vote in currentVotes" class="vote">
-          {{ vote.voter }}
-        </div>
-      </div>
-      <div class="action">
-        <button @click="voteup"
-        class="vote-btn">投票</button>
-      </div>
+      <vote-option
+        :option="option"
+        :allVotes="votes"
+      ></vote-option>
     </div>
   </div>
 </template>
 
 
 <script>
+import VoteOption from './VoteOption'
+
   export default {
     name: 'list',
+    components: { VoteOption },
     data: function () {
       return {
         votes: [
@@ -42,17 +35,9 @@
     computed: {
       options: function () {
         return this.$store.state.vote.options
-      },
-      currentVotes: function () {
-        return this.votes.filter( t => t.optionId === option.id)
       }
     },
-    methods: {
-      voteup: function () {
-        console.log('voteup', this.votes)
-        this.votes.push({ voter: 'aaa' })
-      }
-    },
+
     created: function () {
       console.log('created')
       this.$store.dispatch({
@@ -61,53 +46,3 @@
     }
   }
 </script>
-
-<style scoped>
-  .option {
-    background-color: #fff;
-    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.8);
-    font-size: 20px;
-    text-align: center;
-    line-height: 1.8;
-    padding: 10px;
-    margin-top: 15px;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .option .title {
-    display: flex;
-    justify-content: space-around;
-  }
-  .option .title .text {
-    flex-shrink: 1;
-    margin: 0;
-    font-size: 18px ;
-    border: 2px solid #00bcd4;
-    color: #00bcd4;
-    display: inline-block;
-    padding: 3px 15px;
-  }
-
-  .action {
-    margin-top: 20px;
-    display: flex;
-    justify-content: space-around;
-  }
-  .votes {
-    margin-top: 10px;
-    text-align: left;
-  }
-  .vote {
-    display: inline-block;
-    background-color: #000;
-    color: #fff;
-    padding: 2px;
-    margin-right: 5px;
-    font-size: 14px;
-  }
-  button.vote-btn {
-    font-size: 20px;
-    padding: 6px 12px;
-  }
-</style>
---
+++ src/components/VoteOption.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="option">
+    <div class="title">
+      <div class="text">
+        {{ option.text }}
+      </div>
+    </div>
+    <div class="votes" v-for="vote in votes">
+      {{ vote.voter }}
+    </div>
+    <div class="action">
+      <button @click="voteup"
+      class="vote-btn">投票</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vote-option',
+    props: ['option', 'allVotes'],
+    computed: {
+      votes: function () {
+        return this.allVotes.filter(t => t.optionId == this.option.id)
+      }
+    },
+    methods: {
+      voteup: function () {
+        console.log('voteup', this.option.id)
+        // this.votes.push({ voter: 'aaa' })
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  .option {
+    background-color: #fff;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.8);
+    font-size: 20px;
+    text-align: center;
+    line-height: 1.8;
+    padding: 10px;
+    margin-top: 15px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .option .title {
+    display: flex;
+    justify-content: space-around;
+  }
+  .option .title .text {
+    flex-shrink: 1;
+    margin: 0;
+    font-size: 18px ;
+    border: 2px solid #00bcd4;
+    color: #00bcd4;
+    display: inline-block;
+    padding: 3px 15px;
+  }
+
+  .action {
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-around;
+  }
+  .votes {
+    margin-top: 10px;
+    text-align: left;
+  }
+  .vote {
+    display: inline-block;
+    background-color: #000;
+    color: #fff;
+    padding: 2px;
+    margin-right: 5px;
+    font-size: 14px;
+  }
+  button.vote-btn {
+    font-size: 20px;
+    padding: 6px 12px;
+  }
+</style>
